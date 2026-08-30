c6a32b00196a9ffaaf1a7865a44550627d01e955
whtbl3 <58665234+whtbl3@users.noreply.github.com>
2026-08-30 00:54:31 +0000

    solve: 2 problems (2026-08-30)
    - easy-select-002
    - easy-select-005

---
+++ practice/sql/easy/easy-select-002/solution.sql
@@ -0,0 +1,6 @@
+-- Xom Data · Get product name and price
+-- Problem: https://xomdata.com/practice/easy-select-002
+-- Solved: 2026-08-30
+
+SELECT name, price FROM products
+ORDER BY name;
---
+++ practice/sql/easy/easy-select-005/solution.sql
@@ -0,0 +1,6 @@
+-- Xom Data · Store list by city
+-- Problem: https://xomdata.com/practice/easy-select-005
+-- Solved: 2026-08-30
+
+SELECT name, city FROM stores
+ORDER BY city, name;
